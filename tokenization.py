8a941d8f1f862a6720b2230e67d75b953a8c87b8
Adam Stogsdill <adam.stogsdill@gmail.com>
2024-09-21 03:52:48 -0500

    uploaded more tokenization features

--- tokenization.py
+++ tokenization.py
@@ -1,4 +1,5 @@
 import torch
+import re
 
 def load_token_keys(key_loc="./basic_tokens.txt") -> dict[str, int]:
     token_keys = []
@@ -6,3 +7,8 @@ def load_token_keys(key_loc="./basic_tokens.txt") -> dict[str, int]:
         while line := token_txt.readline():
             token_keys.append(line.strip())
     return {k : i for i, k in enumerate(token_keys)}
+
+def tokenize_sequence(sequence: str, vocab: dict[str, int]) -> list[str]:
+    sequence_list = re.findall(r"(?:<[^>]+>)|(?:.)", sequence)
+    
+    return [vocab[token_key] for token_key in sequence_list]
